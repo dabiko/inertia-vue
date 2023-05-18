db3e575e29394cbfd802bb7422bc31e49c106950
Dabiko Bleaise <dabiko.blaise@gmail.com>
2023-05-18 02:05:53 +0100

    project setup

---
+++ resources/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+</script>
+
+<template>
+    <div>
+        <h1>Hello Dabiko</h1>
+        <button @click="count++">+</button>
+         You clicked me {{ count }} times.
+        <button @click="count--">-</button>
+    </div>
+</template>
